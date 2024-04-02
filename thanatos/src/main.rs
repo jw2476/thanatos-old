a1b77532f0a9c3d6d7b5ecda959dd518f22e9052
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2024-04-02 17:38:25 +0100

    FPS logger

--- thanatos/src/main.rs
+++ thanatos/src/main.rs
@@ -4,6 +4,8 @@ mod event;
 mod graphics;
 mod window;
 
+use std::time::{Duration, Instant};
+
 use crate::{camera::Camera, window::Window};
 use event::Event;
 use glam::{Quat, Vec3};
@@ -24,6 +26,22 @@ struct Tree {
 }
 */
 
+#[derive(Clone, Debug)]
+pub struct Clock {
+    frame_delta: Duration,
+    start: Instant,
+    last: Instant,
+}
+
+impl Clock {
+    pub fn tick(world: &mut World) {
+        let mut clock = world.get_mut::<Clock>().unwrap();
+        let now = Instant::now();
+        clock.frame_delta = now - clock.last;
+        clock.last = now;
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub enum State {
     Stopped,
@@ -37,7 +55,6 @@ async fn main() {
     pretty_env_logger::init();
 
     let window = Window::new();
-
 
     let renderer = Renderer::new(&window).unwrap();
     let camera = Camera::new(&window);
@@ -63,6 +80,11 @@ async fn main() {
         //.with_resource(assets)
         .with_resource(Mouse::default())
         .with_resource(Keyboard::default())
+        .with_resource(Clock {
+            frame_delta: Duration::default(),
+            start: Instant::now(),
+            last: Instant::now(),
+        })
         .with_ticker(window::clear_mouse_delta)
         .with_ticker(window::poll_events)
         .with_handler(camera::handle_resize)
@@ -90,6 +112,11 @@ async fn main() {
             let rotation = Quat::from_rotation_x(-mouse.delta.y * 0.01);
             camera.direction = rotation * camera.direction;
         })
+        .with_ticker(|world| {
+            let clock = world.get::<Clock>().unwrap();
+            println!("FPS: {}", 1.0 / clock.frame_delta.as_secs_f32());
+        })
+        .with_ticker(Clock::tick)
         .with_handler(|world, event| match event {
             Event::Stop => {
                 *world.get_mut::<State>().unwrap() = State::Stopped;
